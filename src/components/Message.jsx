b6ae9b0ff96ceeef17576b56974416fb21d02dc8
Chrstphr <somto565@gmail.com>
2023-06-22 23:18:56 +0200

    done with message comp

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
