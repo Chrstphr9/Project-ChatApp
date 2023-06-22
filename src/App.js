db65a961972438ba71363057d17f1323669985cd
Chrstphr <somto565@gmail.com>
2023-06-22 14:40:01 +0200

    home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './styles.scss'
 import Chat from './components/Chat';
 import Register from './pages/Register';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     <Register />
+     {/* <Home /> */}
+     
     </div>
   );
 }
