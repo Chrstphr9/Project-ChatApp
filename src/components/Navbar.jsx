d2b82a4c312b9cc98887ff2c5c8b7cdaa8f6306b
Chrstphr <somto565@gmail.com>
2023-06-22 22:40:50 +0200

    done with search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">Lama Chat</span>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export const Search = () => {
+const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" placeholder='Find A User'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Search
