a88bbca14d0de5af5994e0c3a3a4007501d0d95e
Chrstphr <somto565@gmail.com>
2023-07-08 19:04:38 +0200

    working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 import Add from "../images/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+
+      await setDoc(doc);
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
-    <div className="formWrapper">
-      <span className="logo">Chrstphr Chat</span>
-      <span className="title">Register</span>
-      <form >
-        <input type="text" placeholder="Display Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input style={{ display: "none" }} type="file" id="file" />
-        <label htmlFor="file">
-          <img src={Add} alt="" />
-          <span>Add An Avatar</span>
-        </label>
-        <button>Sign Up</button>
-        {/* {err && <span>Something Went Wrong</span>} */}
-      </form>
-      <p>You Do Have An Account? Login</p>
+      <div className="formWrapper">
+        <span className="logo">Chrstphr Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add An Avatar</span>
+          </label>
+          <button>Sign Up</button>
+          {err && <span>Something Went Wrong</span>}
+        </form>
+        <p>You Do Have An Account? Login</p>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
